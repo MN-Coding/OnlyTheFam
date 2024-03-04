4f8d7c088ccbf2c1463517c521c0331757ecbbdb
Mark Liu <markliu202@gmail.com>
2024-03-03 23:00:15 -0500

    add tmp getallevents route

--- server/src/main/kotlin/com/example/data/model/Event.kt
+++ server/src/main/kotlin/com/example/data/model/Event.kt
@@ -3,4 +3,4 @@ package com.example.data.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Event(val eventID: String, val name: String, val description: String, val startDatetime: String, val endDatetime: String)
+data class Event(val eventID: String, val name: String, val description: String?, val startDatetime: String, val endDatetime: String)
