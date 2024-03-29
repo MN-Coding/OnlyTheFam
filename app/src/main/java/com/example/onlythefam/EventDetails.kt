4a7bff458d1f793bd0102263636204b415bcfab0
ajleiva <ajleiva4@gmail.com>
2024-03-29 15:00:20 -0400

    Added spacing for event details and added an edit button

--- app/src/main/java/com/example/onlythefam/EventDetails.kt
+++ app/src/main/java/com/example/onlythefam/EventDetails.kt
@@ -1,9 +1,12 @@
 package com.example.onlythefam
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -41,18 +44,37 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import java.net.URLEncoder
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun EventDetails(navController: NavController, eventId: String) {
     val event = remember { mutableStateOf<EventResponse?>(null) }
+    var inEditMode by remember { mutableStateOf(false) }
+    val editedDescription = remember { mutableStateOf("") }
     val allergies = remember { mutableStateOf<List<String>>(listOf()) }
 
     LaunchedEffect(eventId) {
         event.value = getEventById(eventId)
+        editedDescription.value = event.value?.description ?: ""
         allergies.value = getParticipantAllergies(eventId)
     }
 
@@ -71,70 +93,199 @@ fun EventDetails(navController: NavController, eventId: String) {
     ) { innerPadding ->
         event.value?.let { eventDetails ->
             Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
-                Text(text = eventDetails.description, style = MaterialTheme.typography.body1)
-                val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy h:mma")
-                Row(
-                    modifier = Modifier.padding(top = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(Icons.Filled.AccessTime, contentDescription = "Time")
-                    Text(
-                        text = "Start: ${
-                            LocalDateTime.parse(eventDetails.startDatetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).format(
-                                formatter
-                            )
-                        }",
-                        style = MaterialTheme.typography.body1
-                    )
+                if (!inEditMode) {
+                    StaticEventDetails(eventDetails, innerPadding, editedDescription, allergies)
+                } else {
+                    EditEventDetails(eventDetails, innerPadding, editedDescription, allergies)
                 }
-
-                Row(
-                    modifier = Modifier.padding(top = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(Icons.Filled.AccessTime, contentDescription = "Time")
-                    Text(
-                        text = "End: ${
-                            LocalDateTime.parse(eventDetails.endDatetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).format(
-                                formatter
-                            )
-                        }",
-                        style = MaterialTheme.typography.body1
-                    )
+                Spacer(Modifier.height(12.dp))
+                var updateEventDetails = "Update Event Details"
+                var editEventDetails = "Edit Event Details"
+                Button(
+                    onClick = { inEditMode = !inEditMode },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                {
+                    if (inEditMode) {
+                        Text(updateEventDetails)
+                    } else {
+                        Text(editEventDetails)
+                    }
                 }
-                Row(
-                    modifier = Modifier.padding(top = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(Icons.Filled.LocationOn, contentDescription = "Location")
-                    Text(text = "Location: ${eventDetails.location}", style = MaterialTheme.typography.body1)
-                }
-
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = "Participants: ${eventDetails.participants.joinToString(", ")}",
-                    style = MaterialTheme.typography.body1
-                )
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = "Allergies: ${allergies.value.joinToString(", ")}",
-                    style = MaterialTheme.typography.body1
-                )
-                val address = eventDetails.location
-                val encodedAddress = URLEncoder.encode(address, "UTF-8")
-                val mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=$encodedAddress&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7C$encodedAddress&key=AIzaSyCg28OjKgjh8mYsAlrtDhtXF-0L2QMH1_Q"
-                Image(
-                    painter = rememberAsyncImagePainter(mapUrl),
-                    contentDescription = "Event Location Map",
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.Crop
-                )
             }
         }
     }
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun StaticEventDetails(
+    eventDetails: EventResponse,
+    innerPadding: PaddingValues,
+    editedDescription: MutableState<String>,
+    allergies: MutableState<List<String>>
+) {
+    Text(text = editedDescription.value, style = MaterialTheme.typography.body1)
+    val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy h:mma")
+    Spacer(Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.AccessTime, contentDescription = "Time")
+        Text(
+            text = "Start: ${
+                LocalDateTime.parse(
+                    eventDetails.startDatetime,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                ).format(
+                    formatter
+                )
+            }",
+            style = MaterialTheme.typography.body1
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.AccessTime, contentDescription = "Time")
+        Text(
+            text = "End: ${
+                LocalDateTime.parse(
+                    eventDetails.endDatetime,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                ).format(
+                    formatter
+                )
+            }",
+            style = MaterialTheme.typography.body1
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.LocationOn, contentDescription = "Location")
+        Text(text = "Location: ${eventDetails.location}", style = MaterialTheme.typography.body1)
+    }
+    Spacer(Modifier.height(12.dp))
+    Text(
+        modifier = Modifier.padding(top = 8.dp),
+        text = "Participants: ${eventDetails.participants.joinToString(", ")}",
+        style = MaterialTheme.typography.body1
+    )
+    Spacer(Modifier.height(12.dp))
+    Text(
+        modifier = Modifier.padding(top = 8.dp),
+        text = "Allergies: ${allergies.value.joinToString(", ")}",
+        style = MaterialTheme.typography.body1
+    )
+    Spacer(Modifier.height(12.dp))
+    val address = eventDetails.location
+    val encodedAddress = URLEncoder.encode(address, "UTF-8")
+    val mapUrl =
+        "https://maps.googleapis.com/maps/api/staticmap?center=$encodedAddress&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7C$encodedAddress&key=AIzaSyCg28OjKgjh8mYsAlrtDhtXF-0L2QMH1_Q"
+    Image(
+        painter = rememberAsyncImagePainter(mapUrl),
+        contentDescription = "Event Location Map",
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun EditEventDetails(
+    eventDetails: EventResponse,
+    innerPadding: PaddingValues,
+    editedDescription: MutableState<String>,
+    allergies: MutableState<List<String>>
+) {
+    Text(text = "Description:", style = MaterialTheme.typography.body1)
+    TextField(
+        value = editedDescription.value,
+        onValueChange = { editedDescription.value = it }
+    )
+    Spacer(Modifier.height(12.dp))
+    val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy h:mma")
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.AccessTime, contentDescription = "Time")
+        Text(
+            text = "Start: ${
+                LocalDateTime.parse(
+                    eventDetails.startDatetime,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                ).format(
+                    formatter
+                )
+            }",
+            style = MaterialTheme.typography.body1
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.AccessTime, contentDescription = "Time")
+        Text(
+            text = "End: ${
+                LocalDateTime.parse(
+                    eventDetails.endDatetime,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                ).format(
+                    formatter
+                )
+            }",
+            style = MaterialTheme.typography.body1
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Row(
+        modifier = Modifier.padding(top = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(Icons.Filled.LocationOn, contentDescription = "Location")
+        Text(
+            text = "Location: ${eventDetails.location}",
+            style = MaterialTheme.typography.body1
+        )
+    }
+    Spacer(Modifier.height(12.dp))
+    Text(
+        modifier = Modifier.padding(top = 8.dp),
+        text = "Participants: ${eventDetails.participants.joinToString(", ")}",
+        style = MaterialTheme.typography.body1
+    )
+    Spacer(Modifier.height(12.dp))
+    Text(
+        modifier = Modifier.padding(top = 8.dp),
+        text = "Allergies: ${allergies.value.joinToString(", ")}",
+        style = MaterialTheme.typography.body1
+    )
+    Spacer(Modifier.height(12.dp))
+    val address = eventDetails.location
+    val encodedAddress = URLEncoder.encode(address, "UTF-8")
+    val mapUrl =
+        "https://maps.googleapis.com/maps/api/staticmap?center=$encodedAddress&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7C$encodedAddress&key=AIzaSyCg28OjKgjh8mYsAlrtDhtXF-0L2QMH1_Q"
+    Image(
+        painter = rememberAsyncImagePainter(mapUrl),
+        contentDescription = "Event Location Map",
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
 
 private suspend fun getEventById(eventId: String): EventResponse? {
     val client = HttpClient(CIO) {
